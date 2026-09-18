8645056ee364d378b521cfed664f0309ab474afd
shruti <khandareshruti5@gmail.com>
2026-09-18 21:22:53 +0530

    fix api route slash

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,7 +31,7 @@ function App() {
     formData.append("elapsed_hours", elapsedHours);
 
     try {
-      const res = await axios.post("https://oil-spill-detection-system.onrender.com//api/analyze", formData);
+      const res = await axios.post("https://oil-spill-detection-system.onrender.com/api/analyze", formData);
       setResults(res.data);
     } catch (err) {
       console.error(err);
